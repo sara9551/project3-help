f26921ea949b38c58b467c5df0d66529e0db6f4b
Sara Anspach <sara.anspach@gmail.com>
2018-11-04 23:42:50 -0200

    6000 try

--- server.js
+++ server.js
@@ -21,5 +21,5 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactchorelist"
 
 // Start the API server
 app.listen(PORT, function() {
-  console.log('API Server now listening on PORT ${PORT}!`);
+  console.log(`API Server now listening on PORT ${PORT}!`);
 });
